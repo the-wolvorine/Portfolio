8272276b41aecf8b948aa7a07824cd28b504b628
the-wolvorine <uday.cheekatipalli@slu.edu>
2023-07-03 13:12:37 -0500

    Updated header width

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
 
   return (
     <header className="font-sans z-50 bg-white hue-rotate-15 w-screen h-20 fixed shadow-2xl overflow-hidden">
-      <nav className="mx-auto h-20 flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav className="mx-auto h-20 flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="https://uday-cheekatipalli.vercel.app/" className="-m-1.5 p-1.5">
             <span className="sr-only">Uday</span>
